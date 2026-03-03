3775f8f1e59082e472e98245735eb174de7331ee
Lyubov Davletshina <chernonosova.lyubov@yandex.ru>
2026-03-04 00:30:30 +0300

    add modul category

---
+++ src/category.py
@@ -0,0 +1,14 @@
+class Category:
+    name: str          #название
+    description: str   #описание
+    product: list      #список товаров категории
+
+    category_count = 0  #количество категорий
+    product_count = 0   #количество товаров
+
+    def __init__(self, name, description, product):
+        self.name = name
+        self.description = description
+        self.product = product
+        Category.category_count += 1
+        Category.product_count += len(product)
--- src/product.py
+++ src/product.py
@@ -1,8 +1,8 @@
 class Product:
-    name: str
-    description: str
-    price: float
-    quantity: int
+    name: str         #название
+    description: str  #описание
+    price: float      #цена
+    quantity: int     #количество в наличии
 
     def __init__(self, name, description, price, quantity):
         self.name = name
