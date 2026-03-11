e73e04b47f9d2f4f5469efe0ba3ef32b44afa1cc
Lyubov Davletshina <chernonosova.lyubov@yandex.ru>
2026-03-11 23:57:12 +0300

    update module category

--- src/category.py
+++ src/category.py
@@ -1,3 +1,5 @@
+from src.product import Product
+
 class Category:
     name: str  # название
     description: str  # описание
@@ -9,6 +11,45 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+
+    @property
+    def products(self):
+        """Метод, который позволяет выводить список товаров в виде строк в формате:
+        Название продукта, 80 руб. Остаток: 15 шт."""
+        products_str = ""
+        for product in self.__products:
+            products_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity}\n"
+        return products_str
+
+
+    def add_product(self, product: Product):
+        """Метод для добавления продукта в атрибут products"""
+        self.__products.append(product)
+        Category.product_count += 1
+
+
+if __name__ == "__main__":
+    product_1 = Product("Samsung Galaxy C23 Ultra",
+                        "256GB, Серый цвет, 200MP камера",
+                        180000.0,
+                        5)
+
+    product_2 = Product("Iphone 15",
+                        "512GB, Gray space",
+                        210000.0,
+                        8)
+
+    category_1 = Category("Смартфоны","Смартфоны, как средство", [product_1, product_2])
+
+    print(category_1.products)
+    print(category_1.product_count)
+
+    product_3 = Product("Xiaomi Redmi Note 11","1024GB, Синий",31000.0,14)
+    category_1.add_product(product_3)
+    print(category_1.products)
+    print(category_1.product_count)
+
