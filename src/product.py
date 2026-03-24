ab0ebecee28f2a3373fd16ed53a38597fff60da7
Lyubov Davletshina <chernonosova.lyubov@yandex.ru>
2026-03-24 23:56:01 +0300

    add module print_mixin

--- src/product.py
+++ src/product.py
@@ -1,6 +1,7 @@
 from src.base_product import BaseProduct
+from src.print_mixin import PrintMixin
 
-class Product(BaseProduct):
+class Product(BaseProduct, PrintMixin):
     name: str  # название
     description: str  # описание
     price: float  # цена
@@ -11,6 +12,7 @@ class Product(BaseProduct):
         self.description = description
         self.__price = price
         self.quantity = quantity
+        super().__init__()
 
     def __str__(self):
         return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
